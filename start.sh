14f99dc4b5a5f1f169134ca202a4712c728cdb40
wfleurant <williamfleurant@comcast.net>
2015-12-06 00:50:37 -0500

    docker-compose: init scripts and respected config/database vars set

--- start.sh
+++ start.sh
@@ -1,12 +1,55 @@
 #!/usr/bin/env bash
 
-# docker: change file modes: users, groups, others: +write, +read, +execute
-chmod ugo+wrx -R bootstrap/cache/ storage/ vendor/
+#### Variables
 
-# docker: build to add any possible changes to config files in images/
+## composer: github key
+composer_key=Example-kT0uZ2GM3HC0c4Efzx8yWBUSRB2agMsQ
+
+#### Functions
+
+## docker: install laravel dependencies
+function dependencies () {
+
+    composer config -g github-oauth.github.com ${composer_key}
+    composer install --profile
+    composer dump-autoload --optimize --profile
+
+    npm install
+
+    bower install
+
+    export DISABLE_NOTIFIER=true
+    gulp
+
+    grunt
+
+    mkdir -p storage/logs
+    touch storage/logs/laravel.log
+
+    php artisan optimize
+
+    php artisan migrate --database=mysql
+    php artisan migrate:install
+
+}
+
+## docker: change file modes: users, groups, others: +write, +read, +execute
+function chmods () {
+    sudo chmod ugo+wrx -R bootstrap/cache/ storage/ vendor/
+}
+
+
+#### Main
+
+chmods
+
+## docker: build to add any possible changes to config files in ./images/
 build=true
-[ $build ] && docker-compose build || :
+[ $build ] \
+    && docker-compose build \
+    || echo 'docker-compose build: skipped'
 
-# docker: stop and start containers
-docker-compose restart
+## docker: ☘ start containers
+docker-compose stop -t 45
+docker-compose up -d
 docker-compose logs
